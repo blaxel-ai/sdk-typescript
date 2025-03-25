ba150eacc3142d04e4ad3776eebd3ba91f0dbd6a
drappier-charles <cdrappier@beamlit.com>
2025-03-25 18:21:11 +0100

    Refactor langchain instrumentation to use ES module imports instead of CommonJS requires, improving code clarity and consistency.

--- src/instrumentation/langchain.ts
+++ src/instrumentation/langchain.ts
@@ -1,12 +1,13 @@
-/* eslint-disable */
+ 
+import RunnableModule from "@langchain/core/runnables";
+import ToolsModule from "@langchain/core/tools";
+import VectorStoresModule from "@langchain/core/vectorstores";
 import { LangChainInstrumentation } from "@traceloop/instrumentation-langchain";
+import AgentsModule from "langchain/agents";
+import ChainsModule from "langchain/chains";
+
 export function langchain(instrumentor: any) {
   const langchain = instrumentor as LangChainInstrumentation;
-  const RunnableModule = require("@langchain/core/runnables");
-  const ToolsModule = require("@langchain/core/tools");
-  const ChainsModule = require("langchain/chains");
-  const AgentsModule = require("langchain/agents");
-  const VectorStoresModule = require("@langchain/core/vectorstores");
   langchain.manuallyInstrument({
     runnablesModule: RunnableModule,
     toolsModule: ToolsModule,
