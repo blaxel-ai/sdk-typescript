fd4e669ead006943ccd786bc6e3bee26ab612c73
drappier-charles <cdrappier@beamlit.com>
2025-03-25 17:05:07 +0100

    Implement token refresh logic in ClientCredentials and DeviceMode classes, including jwtDecode for token validation. Refactor authenticate method to handle promise management and improve token expiration checks.

--- src/authentication/clientcredentials.ts
+++ src/authentication/clientcredentials.ts
@@ -1,3 +1,4 @@
+import { jwtDecode } from "jwt-decode";
 import { oauthToken } from "../client/authentication.js";
 import { Credentials } from "./credentials.js";
 import { CredentialsType } from "./types.js";
@@ -6,23 +7,46 @@ export class ClientCredentials extends Credentials {
   private clientCredentials: string;
   private accessToken: string;
   private credentials: CredentialsType;
+  private currentPromise: Promise<void> | null;
 
   constructor(credentials: CredentialsType) {
     super();
     this.clientCredentials = credentials.clientCredentials || '';
     this.credentials = credentials
     this.accessToken = ''
+    this.currentPromise = null
   }
 
   get workspace() {
     return this.credentials.workspace || process.env.BL_WORKSPACE || '';
   }
 
-  async authenticate() {
-    await this.refresh()
+  needRefresh() {
+    if (this.currentPromise) return false
+    if (this.accessToken) {
+      const decoded = jwtDecode(this.accessToken)
+      const {exp,iat} = decoded
+      if (!exp || !iat) return true
+      const expDate = new Date(exp * 1000)
+      const iatDate = new Date(iat * 1000)
+      const nowDate = new Date()
+      const diff = expDate.getTime() - nowDate.getTime()
+      const iatDiff = expDate.getTime() - iatDate.getTime()
+      const ratio = diff/iatDiff
+      return ratio < 0.5
+    }
+    return true
   }
 
-  async refresh() {
+  async authenticate() {
+    if (!this.needRefresh()) {
+      return this.currentPromise || Promise.resolve()
+    }
+    this.currentPromise = this.process()
+    return this.currentPromise
+  }
+
+  async process() {
     const response = await oauthToken({
       headers: {
         'Authorization': `Basic ${this.clientCredentials}`
@@ -35,6 +59,7 @@ export class ClientCredentials extends Credentials {
         throw new Error(response.error.error)
     }
     this.accessToken = response.data?.access_token || ''
+    this.currentPromise = null
   }
 
   get authorization() {
@@ -44,5 +69,4 @@ export class ClientCredentials extends Credentials {
   get token() {
     return this.accessToken
   }
-
 }
--- src/authentication/deviceMode.ts
+++ src/authentication/deviceMode.ts
@@ -1,30 +1,51 @@
+import { jwtDecode } from 'jwt-decode';
 import { oauthToken } from "../client/authentication.js";
 import { CredentialsType } from "./types.js";
-
 export class DeviceMode {
   private refreshToken: string;
   private deviceCode: string;
   private accessToken: string;
   private credentials: CredentialsType;
+  private currentPromise: Promise<void> | null;
   // private expireIn: number;
   constructor(credentials: CredentialsType) {
     this.refreshToken = credentials.refresh_token || ''
     this.deviceCode = credentials.device_code || ''
     this.accessToken = credentials.access_token || ''
     this.credentials = credentials
-    // this.expireIn = credentials.expires_in || 7200
+    this.currentPromise = null
   }
 
   get workspace() {
     return this.credentials.workspace || process.env.BL_WORKSPACE || '';
   }
 
-  async authenticate() {
-    await this.refresh()
+  needRefresh() {
+    if (this.currentPromise) return false
+    if (this.accessToken) {
+      const decoded = jwtDecode(this.accessToken)
+      const {exp,iat} = decoded
+      if (!exp || !iat) return true
+      const expDate = new Date(exp * 1000)
+      const iatDate = new Date(iat * 1000)
+      const nowDate = new Date()
+      const diff = expDate.getTime() - nowDate.getTime()
+      const iatDiff = expDate.getTime() - iatDate.getTime()
+      const ratio = diff/iatDiff
+      return ratio < 0.5
+    }
+    return true
   }
 
-  async refresh() {
-    // TODO: Implement expire usage, to avoid calling the API every time we refresh the token
+  async authenticate() {
+    if (!this.needRefresh()) {
+      return this.currentPromise || Promise.resolve()
+    }
+    this.currentPromise = this.process()
+    return this.currentPromise
+  }
+
+  async process() {
     const response = await oauthToken({
       body: {
         grant_type: 'refresh_token',
@@ -36,11 +57,12 @@ export class DeviceMode {
       throw new Error(response.error.error)
     }
     this.accessToken = response.data?.access_token || ''
-    // this.expireIn = response.data?.expires_in || 7200
+    console.log(response.data)
+    this.currentPromise = null
   }
 
   get authorization() {
-      return `Bearer ${this.accessToken}`
+    return `Bearer ${this.accessToken}`
   }
 
   get token() {
