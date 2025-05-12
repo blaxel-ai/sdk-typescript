6b4d422af0d4eedd30b02fe934abc3365eed456d
drappier-charles <cdrappier@blaxel.ai>
2025-05-12 09:12:31 -0700

    Disable JSON logger for now

--- @blaxel/telemetry/src/index.ts
+++ @blaxel/telemetry/src/index.ts
@@ -1,12 +1,10 @@
-import { settings } from "@blaxel/core";
-import { setJsonLogger } from "./json_logger";
 import { setLegacyLogger } from "./legacy_logger";
 import { blaxelTelemetry } from "./telemetry";
 blaxelTelemetry.initialize();
-if (settings.loggerType === "http") {
-  setLegacyLogger();
-} else if (settings.loggerType === "json") {
-  setJsonLogger();
-}
+// if (settings.loggerType === "http") {
+setLegacyLogger();
+// } else if (settings.loggerType === "json") {
+//   setJsonLogger();
+// }
 export { blaxelTelemetry };
 
