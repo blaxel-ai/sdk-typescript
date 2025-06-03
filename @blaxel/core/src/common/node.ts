9b4b56ec8bab3205099a51078f4287787dacb637
drappier-charles <cdrappier@blaxel.ai>
2025-06-02 20:06:59 -0700

    Fix websocket import

--- @blaxel/core/src/common/node.ts
+++ @blaxel/core/src/common/node.ts
@@ -9,6 +9,10 @@ let os: typeof import("os") | null = null;
 let path: typeof import("path") | null = null;
 let dotenv: typeof import("dotenv") | null = null;
 let ws: typeof import("ws") | null = null;
+declare const globalThis: {
+  window: any;
+};
+const isBrowser = typeof globalThis !== "undefined" && globalThis && globalThis.window !== undefined;
 if (isNode) {
   try {
     fs = eval("require")("fs");
@@ -17,9 +21,19 @@ if (isNode) {
     dotenv = eval("require")("dotenv");
     ws = eval("require")("ws");
   } catch (e) {
-    console.warn("fs and os are not available in this environment");
+    console.warn("fs, os, path, dotenv, ws are not available in this environment");
   }
 }
+// cloudflare
+else if (!isBrowser) {
+  try {
+    ws = eval("require")("ws");
+  } catch (e) {
+    console.warn("ws is not available in this environment");
+  }
+}
+
+
 
 export { dotenv, fs, os, path, ws };
 
