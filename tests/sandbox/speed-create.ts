a7caabebff1f2b665929ed2486fe52a7a6e46871
cploujoux <cploujoux@blaxel.ai>
2025-07-25 16:49:58 -0400

    Add custom sandbox test script

--- tests/sandbox/speed-create.ts
+++ tests/sandbox/speed-create.ts
@@ -1,6 +1,6 @@
 import { SandboxInstance, logger } from "@blaxel/core";
 
-const sandboxName = "next-js-21"
+const sandboxName = "next-js-25"
 
 logger.setLogger(console)
 
@@ -14,10 +14,11 @@ async function main() {
       image: "blaxel/dev-base:latest",
       memory: 4096,
     })
+    console.log(`Sandbox create time: ${Date.now() - start}ms`)
     const startWait = Date.now()
     await sandbox.wait()
     console.log(`Sandbox wait time: ${Date.now() - startWait}ms`)
-    console.log(`Sandbox create time: ${Date.now() - start}ms`)
+    console.log(`Sandbox create time after watch: ${Date.now() - start}ms`)
 
     // Verify the files were copied by listing the directory in the sandbox
 
@@ -29,16 +30,6 @@ async function main() {
     console.log(`Ls time: ${Date.now() - startLs2}ms`)
     const end = Date.now()
     console.log(`Time taken: ${end - start}ms`)
-
-    await Promise.resolve(new Promise((resolve) => setTimeout(resolve, 15000)))
-
-    console.log("\nTime to first byte check")
-    const startLs3 = Date.now()
-    console.log(await sandbox.fs.ls('/blaxel'));
-    console.log(`Ls after sleep with cold vm time: ${Date.now() - startLs3}ms`)
-    const startLs4 = Date.now()
-    console.log(await sandbox.fs.ls('/blaxel'));
-    console.log(`Ls after sleep with hot vm time: ${Date.now() - startLs4}ms`)
   } catch (e) {
     console.error("There was an error => ", e);
   }
