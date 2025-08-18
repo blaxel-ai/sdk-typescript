491899373eb0af7c796f6647bbc4becb2457d86d
cploujoux <cploujoux@blaxel.ai>
2025-08-18 10:58:21 -0400

    Add one more loop and more logs to test

--- tests/sandbox/streamLogs.ts
+++ tests/sandbox/streamLogs.ts
@@ -1,42 +1,46 @@
 import { SandboxInstance } from "@blaxel/core"
-import { createOrGetSandbox } from "../utils"
-
-const sandboxName = `sandbox-test-stream-logs`
 
 async function main() {
-    const sandbox = await createOrGetSandbox({ sandboxName })
+    const sandbox = await SandboxInstance.create()
+
+    console.log("sandbox created => ", sandbox.metadata!.name)
 
     const resultExpected = `Hello, world!
 Hello, world!
+Hello, world!
+Hello, world!
+Hello, world!
+Hello, world!
 `
-    
+
     // Start the long-running process
     await sandbox.process.exec({
         name: "test",
-        command: "i=1; while [ $i -le 2 ]; do echo \"Hello, world!\"; sleep 31; i=$((i+1)); done",
+        command: `i=1; while [ $i -le 3 ]; do echo "Hello, world!"; sleep 31; echo "Hello, world!"; i=$((i+1)); done`,
     })
 
     let result = ""
     // Use the auto-reconnecting wrapper
     const { close } = await sandbox.process.streamLogs("test", {
         onLog(log) {
+            console.log(`received log => ${log}`)
             result += log + "\n"
         }
     })
     // Wait for process to finish
-    await new Promise(resolve => setTimeout(resolve, 80000))
+    await sandbox.process.wait("test", { maxWait: 1000000 })
 
-    console.log("result => ", result)
+    console.log("\nresult => ", result)
     console.log("resultExpected => ", resultExpected)
     console.log("result === resultExpected => ", result === resultExpected)
     if (result !== resultExpected) {
         throw new Error("Result does not match expected result")
     }
-    
+
     // Stop the streaming
     close()
 
-    await SandboxInstance.delete(sandboxName)
+    await SandboxInstance.delete(sandbox.metadata!.name!)
 }
 
 main().catch(console.error)
