1c9aa16ee0919ae20856edc11596daca1def4b37
cploujoux <cploujoux@blaxel.ai>
2025-09-04 15:36:45 +0200

    Enables passing options to MCP tools
    Allows passing `options` to MCP tools, which includes setting the timeout (`ms`) and providing metadata (`meta`).
    
    This change provides greater flexibility in configuring and using MCP tools.

--- @blaxel/llamaindex/src/tools.ts
+++ @blaxel/llamaindex/src/tools.ts
@@ -1,10 +1,11 @@
 // @ts-ignore - Required for build time due to missing types in 'llamaindex'
 import { getTool, handleDynamicImportError } from "@blaxel/core";
+import { ToolOptions } from "@blaxel/core/tools/mcpTool";
 import { tool } from "llamaindex";
 
-export const blTool = async (name: string, ms?: number) => {
+export const blTool = async (name: string, options?: ToolOptions | number) => {
   try {
-    const blaxelTool = await getTool(name, ms);
+    const blaxelTool = await getTool(name, options)
     const tools = blaxelTool.map((t) => {
       // @ts-ignore - Required for build time due to missing types in 'llamaindex'
       return tool(t.call.bind(t), {
